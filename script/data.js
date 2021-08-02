eaa8a8e2acce87f0654fb0b29e30022c43742546
那光情 <naguangqing@zhongan.com>
2021-08-02 15:58:56 +0800

    feat: update

--- script/data.js
+++ script/data.js
@@ -12,6 +12,8 @@ const ApiList = {
     // 获取实名信息
     getBaseInfo4wx: '/v1/wechatApplet/h5/getBaseInfo4wx',
     getFamilyMemberList: '/api/whale/v1/wechatApplet/h5/getFamilyMemberList',
+
+    validateSmsCode: '/api/whale/v1/H5/insureOrder/personal/message/validateSmsCode'
 };
 
 window['__MOCK_API_RULES'] = [
@@ -31,14 +33,22 @@ window['__MOCK_API_RULES'] = [
     },
 
     {
+        match: ApiList.validateSmsCode,
+        override: {
+            "code": "0",
+            "message": "成功",
+        }
+    },
+
+    {
         match: ApiList.getBaseInfo4wx,
         override:  {
             "code": "0",
             "message": null,
             "result": {
                 "name": "张老三",
-                "phone": "18317867508",
-                "certNo": "620100198505207574",
+                "phone": "18317857608",
+                "certNo": "110101197907298992",
                 "realName": true,
                 "accountId": "210402355"
             }}
@@ -54,37 +64,37 @@ window['__MOCK_API_RULES'] = [
                 "accountId": 210402355,
                 "name": "张老三",
                 "certType": "I",
-                "certNo": "620100198505207574",
-                "phone": "18317867508",
+                "certNo": "110101197907298992",
+                "phone": "18317857608",
                 "relationToPH": "1",
                 "hasSocialInsurance": null,
                 "hasSmoke": null
             },
             {
-                "id": 280005,
-                "accountId": 210402355,
+                "id": 2800051,
+                "accountId": 2104023551,
                 "name": "张老婆",
                 "certType": "I",
-                "certNo": "110101198007278448",
+                "certNo": "110101197910296760",
                 "phone": null,
                 "relationToPH": "2",
                 "hasSocialInsurance": null,
                 "hasSmoke": null
             },
             {
-                "id": 280003,
-                "accountId": 210402355,
+                "id": 2800052,
+                "accountId": 2104023552,
                 "name": "张小四",
                 "certType": "I",
                 "certNo": "110101200008279207",
                 "phone": null,
-                "relationToPH": "3",
+                "relationToPH": "4",
                 "hasSocialInsurance": null,
                 "hasSmoke": null
             },
             {
-                "id": 280004,
-                "accountId": 210402355,
+                "id": 2800053,
+                "accountId": 2104023553,
                 "name": "张小五",
                 "certType": "I",
                 "certNo": "11010120000927722X",
@@ -94,24 +104,34 @@ window['__MOCK_API_RULES'] = [
                 "hasSmoke": null
             },
             {
-                "id": 280001,
-                "accountId": 210402355,
+                "id": 2800054,
+                "accountId": 2104023554,
                 "name": "张他爹",
                 "certType": "I",
-                "certNo": "110101196007277438",
+                "certNo": "110101196309299413",
                 "phone": null,
                 "relationToPH": "4",
                 "hasSocialInsurance": null,
                 "hasSmoke": null
             },
             {
-                "id": 280002,
-                "accountId": 210402355,
+                "id": 2800055,
+                "accountId": 2104023555,
                 "name": "张他娘",
                 "certType": "I",
-                "certNo": "110101196007279249",
+                "certNo": "110101196310299787",
                 "phone": null,
                 "relationToPH": "4",
+                "hasSocialInsurance": null,
+                "hasSmoke": null
+            }, {
+                "id": 2800056,
+                "accountId": 2104023556,
+                "name": "张三十天",
+                "certType": "I",
+                "certNo": "110101202107019397",
+                "phone": null,
+                "relationToPH": "3",
                 "hasSocialInsurance": null,
                 "hasSmoke": null
             }]}
